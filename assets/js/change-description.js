cb6ca662ec32156ad6782d839ad42a981bc733bb
SergeyDarnopykh <sergey.darnopyh@gmail.com>
2018-03-03 22:47:43 +0300

    Add animation

--- assets/js/change-description.js
+++ assets/js/change-description.js
@@ -1,8 +1,8 @@
 const siteDecription = document.querySelectorAll('#siteDescription');
 const descriptions = [ 'magento developer', 'web developer' ];
-//
-// for (let i = 0; ; i++) {
-//     if (i === descriptions.length) i = 0;
-//
-//     setTimeout(3000, function () { siteDecription.innerHTML = descriptions[i]; })
-// }
+
+for (let i = 0; ; i++) {
+    if (i === descriptions.length) i = 0;
+
+    setTimeout(3000, function () { siteDecription.innerHTML = descriptions[i]; })
+}

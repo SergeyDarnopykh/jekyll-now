4002d2cfde25377b77d7d0304e0f22c08c12aed4
SergeyDarnopykh <sergey.darnopyh@gmail.com>
2018-03-04 00:20:25 +0300

    Add test

---
+++ _posts/2018-3-3-Test-Post.md
@@ -0,0 +1,3 @@
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consequuntur culpa, ducimus ex nesciunt, numquam odit quia ratione recusandae reiciendis saepe suscipit, ut veniam? Consequatur cum in provident sequi similique!
+
+The post itself.
--- assets/js/change-description.js
+++
@@ -1,8 +0,0 @@
-const siteDecription = document.querySelectorAll('#siteDescription');
-const descriptions = [ 'magento developer', 'web developer' ];
-
-for (let i = 0; ; i++) {
-    if (i === descriptions.length) i = 0;
-
-    setTimeout(3000, function () { siteDecription.innerHTML = descriptions[i]; })
-}
